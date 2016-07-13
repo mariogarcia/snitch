2e6fa35071320b2ec73d2c14649ac2ed0ceb6881
Mario Garcia <mario.ggar@gmail.com>
2016-07-14 01:15:08 +0200

    Add Spark to tools and first scala Spark task

--- docker/dev/download-spark.sh
+++ docker/dev/download-spark.sh
@@ -2,6 +2,6 @@
 
 mirror=$(curl --stderr /dev/null https://www.apache.org/dyn/closer.cgi\?as_json\=1 | jq -r '.preferred')
 echo "${mirror}"
-url="${mirror}spark/spark-${SPARK_VERSION}/spark-${SPARK_VERSION}.tgz"
+url="${mirror}spark/spark-${SPARK_VERSION}/spark-${SPARK_VERSION}-bin-hadoop2.4.tgz"
 echo "${url}"
 wget "${url}" -O "/tmp/spark_${SPARK_VERSION}.tgz"
