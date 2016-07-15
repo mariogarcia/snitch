bb4edcea6df34bbc6d09c2a8c8838c40951926ee
Mario Garcia <mario.ggar@gmail.com>
2016-07-15 08:10:07 +0200

    Add asciidoctor documentation

--- dev/soho/soho-tweet-stats/src/main/scala/soho/twitter/TweetWordCount.scala
+++ dev/soho/soho-tweet-stats/src/main/scala/soho/twitter/TweetWordCount.scala
@@ -4,29 +4,36 @@ import org.apache.spark.SparkConf
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.kafka._
 
+import scala.util.parsing.json._
+
 object TweetWordCount {
   def main(args:Array[String]) {
     val Array(zkQuorum, group, topics, numThreads) = args
-    val conf = new SparkConf().setAppName("wordCount")
 
+    val conf = new SparkConf().setAppName("wordCount")
     val ssc = new StreamingContext(conf, Seconds(2))
-    val topic = List("twitter-FelizJueves").map((_,2)).toMap
+    val topic = List("twitter-" + topics).map((_,2)).toMap
     val lines = KafkaUtils.createStream(ssc, zkQuorum, group, topic).map(_._2)
 
     ssc.checkpoint("/home/dev/tweets/")
 
-    val words = lines.flatMap(_.split(" "))
-    val counts = words
+    val wordCount = lines
+      .map(tweet => getContentFrom(tweet))
+      .flatMap(word => word.split(" "))
       .map(word => (word, 1))
-      .reduceByKeyAndWindow(_ + _,
-                            _ - _,
-                            Minutes(10),
-                            Seconds(2),
-                            2)
+      .reduceByKeyAndWindow(_ + _, _ - _, Minutes(10), Seconds(2), 2)
 
-    counts.saveAsTextFiles("/home/dev/tweets/tweet-words")
+    wordCount.saveAsTextFiles("/home/dev/tweets/tweet-words")
 
     ssc.start()
     ssc.awaitTermination()
+  }
+
+  def getContentFrom(tweet: String) :String = {
+    return JSON
+      .parseFull(tweet)
+      .get
+      .asInstanceOf[Map[String, Any]] ("text")
+      .asInstanceOf[String]
   }
 }
